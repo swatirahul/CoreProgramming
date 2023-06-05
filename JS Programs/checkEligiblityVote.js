2757ff7c4a4a4718c3defc9ace845f7c2bd79f20
swati gawande <swatigawande2603@gmail.com>
2023-06-05 18:16:54 +0530

    checkEligiblityVote

---
+++ JS Programs/checkEligiblityVote.js
@@ -0,0 +1,11 @@
+var gender = "male";
+    var age = 18;
+            if( gender =="male" && age > 25 && age < 60){
+                console.log("eligible for vote");
+               } 
+        
+        else{
+            console.log("not eligible for vote");
+           
+        }
+    
